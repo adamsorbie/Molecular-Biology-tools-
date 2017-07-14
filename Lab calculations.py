1c0a711c6316b9e58810137e459a731e88e55130
adamsorbie <adam.sorbie@tum.de>
2017-07-14 16:08:41 +0200

    Added molarity/moles calculators

--- Lab calculations.py
+++ Lab calculations.py
@@ -5,6 +5,17 @@ def dil_vol_calc(a, b, c):
     x = (a * b) / c
     return x
 
+
+def moles(m, v):
+    n = m * v
+    return n
+
+
+def mass_grams(n, mw):
+    mass = n * mw
+    return mass
+
+
 calc = str(raw_input("Enter calculation type: "))
 units = (raw_input("Enter units: "))
 list_units = ["l", "ml", "ul", "nl"]
@@ -21,6 +32,18 @@ if "dilution" or "dilution " or "dilutions" or "Dilution" in calc:
     ans = dil_vol_calc(V2, C2, C1)
     diluent = V2 - ans
     print("%.3f" % ans + str(units) + ", diluent =" + " " + "%.3f" % diluent + units)
+
+if "molarity" or "Molarity" or "molar" or "Molar" in calc:
+    m = float(raw_input("Enter concentration in molar: "))
+    v = float(raw_input("Enter volume in L: "))
+    ans = moles(m, v)
+    print(ans)
+
+if "mass" or "Mass" in calc:
+    n = float(raw_input("Enter number of moles: "))
+    mw = float(raw_input("Enter molecular weight: "))
+    ans = mass_grams(n, mw)
+    print(ans)
 
 else:
     print("Error")
