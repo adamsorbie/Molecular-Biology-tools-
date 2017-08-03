897e22a6f56a62f54c761f7496467f485b83e722
adamsorbie <adam.sorbie@tum.de>
2017-08-03 15:01:32 +0200

    added numerical choices

--- Lab calculations.py
+++ Lab calculations.py
@@ -16,7 +16,7 @@ def mass_grams(mass, mw):
     return mass
 
 
-options = ["Dilution", "Mass", "Moles"]
+options = ["Dilution (1)", "Mass (2)", "Moles (3)"]
 
 print("Available calculations: ")
 
@@ -24,7 +24,7 @@ for x in options:
     print(x)
 
 
-calc = str(raw_input("Enter calculation type: "))
+calc = str(raw_input("Enter number of desired calculation: "))
 
 units = (raw_input("Enter units: "))
 
@@ -37,23 +37,23 @@ if units not in list_units:
 print("Make sure to use the same units in the calculation!")
 
 
-if calc == "Dilution":
+if calc == "1":
     C1 = float(raw_input("Enter C1: "))
-    v2 = float(raw_input("Enter V2: "))
+    V2 = float(raw_input("Enter V2: "))
     C2 = float(raw_input("Enter C2: "))
     ans = dil_vol_calc(V2, C2, C1)
     diluent = V2 - ans
     print("%.3f" % ans + str(units) + ", diluent =" + " " + "%.3f" % diluent + units)
     sys.exit()
 
-elif calc == "Mass":
+elif calc == "2":
     m = float(raw_input("Enter concentration in molar: "))
     v = float(raw_input("Enter volume in L: "))
     ans = no_of_moles(m, v)
     print(ans)
     sys.exit()
 
-elif calc == "Moles":
+elif calc == "3":
     no_moles = float(raw_input("Enter number of moles: "))
     molecular_weight = float(raw_input("Enter molecular weight: "))
     ans = mass_grams(no_moles, molecular_weight)
