88cfb4dc9da57068e2423f8ae9c9afb472feedfd
adamsorbie <adam.sorbie@tum.de>
2017-08-03 12:16:58 +0200

    fixed bug where program continued after calculation

--- Lab calculations.py
+++ Lab calculations.py
@@ -32,18 +32,21 @@ if "dilution" or "dilution " or "dilutions" or "Dilution" in calc:
     ans = dil_vol_calc(V2, C2, C1)
     diluent = V2 - ans
     print("%.3f" % ans + str(units) + ", diluent =" + " " + "%.3f" % diluent + units)
+    sys.exit()
 
 if "molarity" or "Molarity" or "molar" or "Molar" in calc:
     m = float(raw_input("Enter concentration in molar: "))
     v = float(raw_input("Enter volume in L: "))
     ans = moles(m, v)
     print(ans)
+    sys.exit()
 
 if "mass" or "Mass" in calc:
     n = float(raw_input("Enter number of moles: "))
     mw = float(raw_input("Enter molecular weight: "))
     ans = mass_grams(n, mw)
     print(ans)
+    sys.exit()
 
 else:
     print("Error")
